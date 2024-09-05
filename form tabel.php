ce7fde84df584aac0d0732d8ebde64c1add30de2
Ainil501 <liniawangy@gmail.com>
2024-09-05 09:17:37 +0700

    memperbarui form tabel

--- form tabel.php
+++ form tabel.php
@@ -8,11 +8,9 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-<<<<<<< HEAD
+
     <title> Formulir Serah Terima Barang</title>
-=======
-    <title>Formulir Serah Terima Barang</title>
->>>>>>> 2f9596e278c3c713f2ebc3110e4070fc4160ad28
+
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -76,13 +74,13 @@
             cursor: pointer;
         }
 
-<<<<<<< HEAD
+
         @media (max-width: 768px) {
             .form-container {
                 padding: 15px;
                 max-width: 100%;
             }
-=======
+
         .table-container {
             margin-top: 20px;
             width: 100%;
@@ -101,7 +99,7 @@
 
         th {
             background-color: #f2f2f2;
->>>>>>> 2f9596e278c3c713f2ebc3110e4070fc4160ad28
+
         }
     </style>
 </head>
@@ -110,19 +108,6 @@
     <div class="container">
         <!-- Form Section -->
         <div class="row justify-content-center">
-<<<<<<< HEAD
-            <div class="col-xl-10 col-lg-12 col-md-12">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="p-5">
-                                    <button class="btn btn-danger mr-1 mb-5" onclick="window.location.href='periode.php'">Tutup Transaksi</button>
-                                    <button class="btn btn-primary ml-2 mb-5" onclick="window.location.href='tabel.php'">Kembali</button>
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Formulir Serah Terima Barang</h1>
-                                    </div>
-=======
             <div class="col-lg-8">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
@@ -132,7 +117,7 @@
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Formulir Serah Terima Barang</h1>
                             </div>
->>>>>>> 2f9596e278c3c713f2ebc3110e4070fc4160ad28
+
 
                             <!-- PHP for handling form submission -->
                             <?php
@@ -254,64 +239,9 @@
             </div>
         </div>
 
-        <!-- Table Section -->
-        <div class="row justify-content-center">
-            <div class="col-lg-12">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <div class="p-5 table-container">
-                            <h3>Data Serah Terima Barang</h3>
-
-                            <?php
-                            // Retrieve data for the table
-                            $conn = new mysqli("localhost", "root", "", "masterruangan");
-
-                            if ($conn->connect_error) {
-                                die("Koneksi gagal: " . $conn->connect_error);
-                            }
-
-                            $result = $conn->query("SELECT * FROM form_serah_terima ORDER BY id DESC");
-
-                            if ($result->num_rows > 0) : ?>
-                            <table class="table table-striped table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Tanggal</th>
-                                        <th>Ruangan</th>
-                                        <th>Jenis</th>
-                                        <th>Jumlah</th>
-                                        <th>Keterangan</th>
-                                        <th>Tanda Tangan</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    $no = 1;
-                                    while ($row = $result->fetch_assoc()) : ?>
-                                        <tr>
-                                            <td><?php echo $no++; ?></td>
-                                            <td><?php echo $row['tanggal']; ?></td>
-                                            <td><?php echo $row['ruangan']; ?></td>
-                                            <td><?php echo $row['jenis']; ?></td>
-                                            <td><?php echo $row['jumlah']; ?></td>
-                                            <td><?php echo $row['keterangan']; ?></td>
-                                            <td><img src="<?php echo $row['ttd']; ?>" alt="Tanda Tangan" style="width: 100px;"></td>
-                                        </tr>
-                                    <?php endwhile; ?>
-                                </tbody>
-                            </table>
-
-                            <?php else : ?>
-                                <p>Belum ada data yang disimpan.</p>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+ 
     </div>
-<<<<<<< HEAD
+
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -360,8 +290,7 @@
             return true;
         }
     </script>
-=======
->>>>>>> 2f9596e278c3c713f2ebc3110e4070fc4160ad28
+
 </body>
 
 <script>
