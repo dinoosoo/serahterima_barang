33974590e5e44423cd9752d5e357b3ea36f0013c
dinoosoo <xxrdha@gmail.com>
2024-10-14 11:59:00 +0700

    memperbarui file

--- edit_form_tabel.php
+++ edit_form_tabel.php
@@ -101,7 +101,7 @@
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="p-5">
-                                <button class="btn btn-primary ml-2 mb-5" onclick="window.location.href='tabeldetail.php?id=<?php echo $_GET['lokasi'];  ?>&jenis_berkas=<?php echo $_GET['jenis_berkas'];  ?>&status=<?php echo $_GET['status'];  ?>'"Back></button>                                   
+                                <button class="btn btn-primary ml-2 mb-5" onclick="window.location.href='tabeldetail.php?id=<?php echo $_GET['lokasi'];  ?>&jenis_berkas=<?php echo $_GET['jenis_berkas'];  ?>&status=<?php echo $_GET['status'];  ?>'">Back</button>                                   
                                     <?php
                                         $conn = new mysqli("localhost", "root", "", "magang_syamrabu");
 
